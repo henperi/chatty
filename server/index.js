e6cffee02a025cbff746008144c405cd18d546bb
Henry <pienry3g@gmail.com>
2019-04-17 15:53:42 +0100

    chore(database): setup database with sqlite3 and mokcs
    - create database models and connectors
    - create graphql resolvers
    - create graphql schemas

--- server/index.js
+++ server/index.js
@@ -1,15 +1,9 @@
-import { ApolloServer, gql } from 'apollo-server';
+import { ApolloServer } from 'apollo-server';
+import { typeDefs } from './data/schema';
+import { mocks } from './data/mocks';
+import { resolvers } from './data/resolvers';
 
 const PORT = 8080;
 
-/**
- * Basic Import
- */
-const typeDefs = gql`
-  type Query {
-    testString: String
-  }
-`;
-
-const server = new ApolloServer({ typeDefs, mocks: true });
+const server = new ApolloServer({ resolvers, typeDefs, mocks });
 server.listen({ port: PORT }).then(({ url }) => console.log(`🚀 Server ready at ${url}`));
